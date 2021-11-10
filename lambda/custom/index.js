f75f12927f2eac258d52a9ad72938f17949969e7
ta2min <ta2mi.prostudy@gmail.com>
2021-11-10 23:39:48 +0900

    デプロイできなくなったので新しいリポジトリに引っ越し

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,26 +2,70 @@
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
+let AWS = require('aws-sdk');
+AWS.config.update({ region: 'ap-northeast-1' });
+const csv = require('csv-parse/lib/sync')
+const numeral = require('@geolonia/japanese-numeral')
+
+let s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+
+function S3_getObject(bucketParams) {
+    return new Promise((resolve, reject) => {
+        s3.getObject(
+            bucketParams,
+            (err, response) => {
+              if (err) {
+                console.log(err);
+                reject(err);
+              }
+                else { resolve(response); }
+            }
+        );
+    });
+}
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
       return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
-      const speechText = 'Welcome, you can say Hello or Help. Which would you like to try?';
+      const speechText = '電気代ぼっとです';
       return handlerInput.responseBuilder
         .speak(speechText)
         .reprompt(speechText)
         .getResponse();
     }
 };
-const HelloWorldIntentHandler = {
+const ElectricBillIntentHandler = {
     canHandle(handlerInput) {
       return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+        && handlerInput.requestEnvelope.request.intent.name === 'ElectricBillIntent';
     },
-    handle(handlerInput) {
-      const speechText = 'Hello World!';
+    async handle(handlerInput) {
+      const bucketName = 'electric-fee-453141222696-ap-notheast-1';
+      const keyName = 'hour_billing.csv'
+      const bucketParams = {
+        Bucket: bucketName,
+        Key: keyName,
+      };
+      let response = await S3_getObject(bucketParams);
+      let text = response.Body.toString("utf-8");
+      const records = csv(text, {
+        columns: true,
+      });
+      const total = records.reduce((sum, record,) => {
+        console.log(sum, record['2021年10月10日使用量'], typeof (parseFloat(record['2021年10月10日使用量'])))
+        if (record['2021年10月10日使用量'] !== '') {
+          return sum + parseFloat(record['2021年10月10日使用量']);
+        } else {
+          return sum;
+        }
+      }, 0);
+      const unit_price = 19.77
+      const kanjiDayTotal = numeral.number2kanji(Math.ceil(total * unit_price))
+      const speechText = `${kanjiDayTotal}円です`
+      console.log(kanjiDayTotal)
       return handlerInput.responseBuilder
         .speak(speechText)
         //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
@@ -108,7 +152,7 @@ const ErrorHandler = {
 exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
     LaunchRequestHandler,
-    HelloWorldIntentHandler,
+    ElectricBillIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler,
@@ -116,3 +160,4 @@ exports.handler = Alexa.SkillBuilders.custom()
   .addErrorHandlers(
     ErrorHandler)
   .lambda();
+  
